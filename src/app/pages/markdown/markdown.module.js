b8f6f8ecd1a41abfd5e682ff584fb078b67e30f8
quyendq.dtag <quyendq.dtag@gmail.com>
2018-07-07 15:43:14 +0700

    init

---
+++ src/app/pages/markdown/markdown.module.js
@@ -0,0 +1,31 @@
+/**
+ * @author v.lugovsky
+ * created on 16.12.2015
+ */
+(function () {
+    'use strict';
+
+    angular.module('BlurAdmin.pages.markdown', ['ngSanitize', 'mdMarkdownIt'])
+        .config(routeConfig)
+        .config(['markdownItConverterProvider', function (markdownItConverter) {
+            markdownItConverter.config('commonmark', {
+                breaks: true,
+                html: true
+            });
+        }]);
+
+    /** @ngInject */
+    function routeConfig($stateProvider) {
+        $stateProvider
+            .state('markdown', {
+                url: '/markdown',
+                templateUrl: 'app/pages/markdown/markdown.html',
+                title: 'markdown',
+                sidebarMeta: {
+                    icon: 'ion-android-home',
+                    order: 0,
+                },
+            });
+    }
+
+})();
